9e1d426cf23175a2d2ecf573777945c6f6f8d025
Nains <jason20226@gmail.com>
2021-08-09 14:45:57 +0800

    2021-08-09 14:45

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,17 @@ import { Component, OnInit, HostListener } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove(e) {
-    this.parallax(e)
-  }
+  @HostListener('document:scroll', ['$event']) onMouseMove(e) { this.parallax(e) }
   constructor() { }
   parallax(e) {
-    console.log(e);
+    // console.log(e);
     Array.from(document.querySelectorAll(".layer")).forEach((layer: any) => {
-      let x = (window.innerWidth - e.pageX * 2) / 2;
-      layer.style.transform = `translateX(${x}px)`;
+      // mousemove
+      // let x = (window.innerWidth - e.pageX * 2) / 2;
+      // layer.style.transform = `translateX(${x}px)`;
+      let height = e.srcElement.scrollingElement.scrollHeight - e.srcElement.scrollingElement.clientHeight;
+      let scrollbar = (e.srcElement.scrollingElement.scrollTop / height) * 100;
+      layer.style.transform = `translateX(${(scrollbar < 50) ? (0 - (50 - scrollbar)) : Math.abs(50 - scrollbar)}%)`;
     })
   }
   ngOnInit() {
